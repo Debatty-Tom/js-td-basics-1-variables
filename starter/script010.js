813b95cc94d26f8b5117562b294be9469ba8b189
tomdebatty <tom.debatty@hotmail.be>
2023-10-04 19:01:30 +0200

    add ex10

--- starter/script010.js
+++ starter/script010.js
@@ -4,7 +4,11 @@
 
 // 1. Déclarer les variables now, ageJohn, ageMark, yearJohn et yearMark
 // puis affecter aux trois premières les valeurs 2019, 28 et 33
-
+const now = 2019;
+const ageJohn = 28;
+const ageMark = 33;
+const yearJohn = now - ageJohn;
+const yearMark = now - ageMark;
 
 // Opérateurs mathématiques
 
@@ -13,13 +17,18 @@
 // idem pour yearMark
 // puis afficher la date de naissance de John 
 // et "Mark est né en …"
-
+console.log(yearJohn);
+console.log(`MArk est né en ${yearMark}`);
 
 // 3. Afficher l'année à laquelle nous serons dans deux ans
 // le double de l'année
 // le dixième de l'année
-
-
+let newYear = now+2;
+console.log(newYear);
+newYear = now*2;
+console.log(newYear);
+newYear = now/10;
+console.log(newYear);
 
 // Opérateurs logiques
 
