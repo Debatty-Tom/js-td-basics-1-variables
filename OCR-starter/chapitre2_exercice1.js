dea1907c51e1196f2025a747b3ee5437b19126e5
tomdebatty <tom.debatty@hotmail.be>
2023-10-04 18:36:13 +0200

    add ex4

--- OCR-starter/chapitre2_exercice1.js
+++ OCR-starter/chapitre2_exercice1.js
@@ -13,3 +13,14 @@
 - Devinez les valeurs à présent contenues dans chacune de ces 7 variables ;
 - Affichez les valeurs des 7 variables dans la console
 */
+let a = 2;
+a -= 1;
+a += 2;
+let b = 8;
+b++;
+const c = a + (b * b);
+const d = a * b + b;
+const e = a * (b + b);
+const f = a * b / a;
+const g = b / a * a;
+console.log(a, b, c ,d ,e, f, g)
