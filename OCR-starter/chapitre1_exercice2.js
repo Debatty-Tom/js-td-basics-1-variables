ccad6e25ed6b21725efb84afdbc47c703f87c615
tomdebatty <tom.debatty@hotmail.be>
2023-10-04 17:56:27 +0200

    add ex2

--- OCR-starter/chapitre1_exercice2.js
+++ OCR-starter/chapitre1_exercice2.js
@@ -9,3 +9,9 @@ Affichez dans la console les opérations de base avec les nombres 6 et 3 :
 Avant d'exécuter le code, devinez à chaque fois le résultat de l'opération
 */
 
+const somme = 6+3;
+const difference = 6-3;
+const multiplication = 6*3;
+const division = 6/3;
+const modulo = 6%3
+console.log(somme, difference, multiplication, division, modulo)
